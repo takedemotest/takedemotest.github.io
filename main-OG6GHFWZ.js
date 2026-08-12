819cbdf0d2eeda4fac74b4a82366fe4e49ba7c64
takedemotest <takedemotest@gmail.com>
2026-08-12 19:01:51 +0000

    Deploying to gh-pages from @ takedemotest/takedemotest.github.io@d3d9275198703143fee90538ef1c0e245296ac0b 🚀

---
+++ main-OG6GHFWZ.js
@@ -0,0 +1,1 @@
+import{a as t}from"./chunk-XNFUFXXQ.js";import"./chunk-4CLCTAJ7.js";t({farmOperations:"https://takedemotest.github.io/farm-operations/remoteEntry.json",inventory:"https://takedemotest.github.io/inventory/remoteEntry.json",expenseRevenue:"https://takedemotest.github.io/expense-revenue/remoteEntry.json"}).then(e=>import("./chunk-QPBMAB2J.js")).catch(e=>console.error(e));
